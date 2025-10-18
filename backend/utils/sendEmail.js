f3910f82e58602a0147514b290d95ddd6615f377
Peter Brooks <digitalyinvestassets@gmail.com>
2025-10-18 02:00:03 +0100

    runing logo error. added logo to metadata og image

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -21,7 +21,7 @@ const sendEmail = async (subject, send_to, template, reply_to, cc) => {
       name: "syntridigital Platform",
       link: "https://syntridigital.com/",
       // Optional product logo
-      logo: 'https://syntridigital.com/logo.png',
+      logo: 'https://res.cloudinary.com/dikwlne6g/image/upload/v1760749070/logo_ct4na9.png',
        // Custom logo height
       logoHeight: '70px'
     },
